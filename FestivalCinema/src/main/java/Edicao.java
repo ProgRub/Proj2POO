04520c8a77642d1f314a20ce1479f11ae0544946
silvia-622 <61243115+silvia-622@users.noreply.github.com>
2020-04-04 16:50:03 +0100

    .

--- FestivalCinema/src/main/java/Edicao.java
+++ FestivalCinema/src/main/java/Edicao.java
@@ -17,9 +17,47 @@ public class Edicao {
     
     public String toString(){
         String Edicao;
-        Edicao = "Edição: " + numEdicao + " Ano: " + ano;
- 
+        Edicao = "Edição: " + numEdicao + " Ano: " + ano + "\n";
+        
+        while(posicao < premios.size()){
+            
+            if (posicao==0){
+                Edicao += "Prémio: Melhor filme \n";
+            }
+            
+            if (posicao==1){
+                Edicao += "Prémio: Melhor ator principal \n";
+            }
+            
+            if (posicao==2){
+                Edicao += "Prémio: Melhor atriz principal \n";
+            }
+            
+            if (posicao==3){
+                Edicao += "Prémio: Melhor ator secundário \n";
+            }
+            
+            if (posicao==4){
+                Edicao += "Prémio: Melhor atriz secundária \n";
+            }
+            
+            if (posicao==5){
+                Edicao += "Prémio: Melhor realizador \n";
+            }
+            
+            if (posicao==6){
+                Edicao += "Prémio: Melhor argumento \n";
+            }
+            
+            if (posicao==7){
+                Edicao += "Prémio: Melhor cinematografia \n";
+            }
+            
+        }
+      
         return Edicao; 
     }
+    
+    
 }
     
