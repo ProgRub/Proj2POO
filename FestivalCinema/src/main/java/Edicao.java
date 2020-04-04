fc94357ab024867ebdaf61ab1fc53297b25fd4d6
silvia-622 <61243115+silvia-622@users.noreply.github.com>
2020-04-04 15:54:04 +0100

    edicao e premio

---
+++ FestivalCinema/src/main/java/Edicao.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+        
+public class Edicao {
+    
+    private int numEdicao;
+    private int ano;
+    ArrayList <String> filmes;
+    ArrayList <String> premio;    
+}
