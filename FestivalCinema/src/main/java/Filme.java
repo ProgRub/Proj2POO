2d4a198fed8a6055ad9cc4586f0b6fadee897cdd
Diego Briceño <diegobriceno13@gmail.com>
2020-04-04 16:03:07 +0100

    Filme

--- FestivalCinema/src/main/java/Filme.java
+++ FestivalCinema/src/main/java/Filme.java
@@ -1,7 +1,45 @@
+import java.util.ArrayList;
 
 public class Filme {
+    private String nome;
     private String genero;
     private int edição;
     private Pessoa realizador;
+    private ArrayList<Pessoa> atores;
+    private int posição = 0;
+    
+    public Filme(String nome, String genero, int edição, Pessoa realizador, ArrayList<Pessoa> atores){
+        this.nome = nome;
+        this.genero = genero;
+        this.edição = edição;
+        this.realizador = realizador;
+        this.atores = atores;
+    }
+    
+    public String toString(){
+        String filme;
+        filme = "Filme: "+nome+"\n";
+        filme += "Género: "+genero+"\n";
+        filme += "Edição: "+edição+"\n";
+        filme += "Realizador: "+"GET NOME PESSOA"+"\n"; //falta fazer
+        while(posição < atores.size()){
+            if(posição == 0){
+                 filme += "Ator Principal: "+"GET NOME ATOR"+"\n";
+            }
+            else if(posição == 1){
+                 filme += "Atriz Principal: "+"GET NOME ATOR"+"\n";
+            }
+            else if (posição == 2){
+                filme += "Atores Secundários: \n";
+                filme += "GET NOME ATOR \n";
+            }
+            else{
+                filme += "GET NOME ATOR \n";
+            }
+            posição++;
+        }
+        
+        return filme;
+    }
     
 }
--- FestivalCinema/src/main/java/Pessoa.java
+++ FestivalCinema/src/main/java/Pessoa.java
@@ -3,4 +3,10 @@ public class Pessoa {
     private String ultimoNome;
     private String genero;
     
+    public Pessoa(String primeiroNome, String ultimoNome, String genero){
+        this.primeiroNome = primeiroNome;
+        this.ultimoNome = ultimoNome;
+        this.genero = genero;
+    }
+
 }
