2bb7c7e9bfbe41090ab731ec135a7ae0c49a37e9
silvia-622 <61243115+silvia-622@users.noreply.github.com>
2020-04-05 01:51:49 +0100

    criação e consulta de edicoes

--- FestivalCinema/src/main/java/Edicao.java
+++ FestivalCinema/src/main/java/Edicao.java
@@ -5,19 +5,25 @@ public class Edicao {
     private int ano;
     private ArrayList <Filme> filmes;
     private ArrayList <String> premios;   
-    private int posicao = 0;
+    private int posicao;
+    private int posicao1;
 
 
-    public Edicao(int numEdicao, int ano,ArrayList <String> premios ){
+    
+    public Edicao(int numEdicao, int ano){
 
     this.numEdicao = numEdicao;
     this.ano = ano;
-    this.premios = premios;
+    premios = new ArrayList <String>();
+    filmes = new ArrayList <Filme>();
+    posicao = 0; 
+    posicao1 = 0;
+    inserePremios();
     }
     
     public String toString(){
         String Edicao;
-        Edicao = "Edição: " + numEdicao + " Ano: " + ano + "\n";
+        Edicao = "Edição: " + numEdicao + " | Ano: " + ano;
         
         return Edicao; 
     }
@@ -32,5 +38,35 @@ public class Edicao {
         premios.add("Melhor argumento");
         premios.add("Melhor cinematografia");
     }
+    
+    public void imprimeFilmes(){
+        System.out.println("FILMES:");
+        if (filmes.isEmpty()){
+            System.out.println("! NÃO HÁ FILMES REGISTADOS !");
+        }
+        else{
+            while (posicao < filmes.size()){
+            System.out.println(filmes.get(posicao));
+            posicao++;  
+            }
+        } 
+    }
+    
+    public void imprimePremios(){
+        System.out.println("PRÉMIOS:");
+        while (posicao1 < premios.size()){
+            System.out.println(premios.get(posicao1));
+            posicao1++;
+        }
+    }
+    
+    public void insereFilmes(Filme filme){
+        filmes.add(filme);   
+    }
+    
+    public int getNumEdicao(){
+        return numEdicao;
+    }
+    
 }
     
--- FestivalCinema/src/main/java/Main.java
+++ FestivalCinema/src/main/java/Main.java
@@ -3,13 +3,19 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-
+    static ArrayList<Edicao> edicoes = new ArrayList();
+    
     public static void main(String[] args) {
         boolean novo = true;
         ArrayList<Ator> atores = new ArrayList();
+        
+        int indexEdicoes = 0;
         int numEdicao = 1;
+        int ano = 3000;
         System.out.println("\t\t\tFESTIVAL CINEMA");
-        System.out.print("(n): Começar um novo programa\t(c): Carregar um programa anterior\nOpção: ");
+        boolean sair = false;
+        while (!sair){
+            System.out.print("(n): Começar um novo programa\t(c): Carregar um programa anterior\nOpção: ");
         Scanner scan = new Scanner(System.in, "cp1252");
         String opcao = scan.nextLine();
         while (opcao.length() > 1) {
@@ -21,18 +27,21 @@ public class Main {
             novo = false;
         }
         boolean quebra = false;
+         
         while (!quebra) {
             System.out.println("*********************");
-            System.out.print("\t\t\tOpções\n(f): Criar Filme\t(a): Criar Ator\t(p) Atribui Papel\t(s): Sair\nOpção: ");
+            System.out.print("\t\t\tOpções\n(f): Criar Filme\t(a): Criar Ator\t(p): Atribui Papel\t(s): Sair\t(h): Criar Edição\t (i): Consultar Edição\nOpção: ");
             opcao = scan.nextLine();
             while (opcao.length() > 1) {
                 System.out.println("Por favor selecione uma das opções disponíveis.");
                 System.out.print("(f): Criar Filme\t(a): Criar Ator\nOpção: ");
                 opcao = scan.nextLine();
             }
-            switch (opcao.toLowerCase()) {
+            switch (opcao) {
                 case "f":
-                    System.out.print("NOVO FILME\nNome do Filme: ");
+                    System.out.print("NOVO FILME\nEdicão do Filme: ");
+                    int edicaoFilme = scan.nextInt();
+                    System.out.print("Nome do Filme: ");
                     String nome = scan.nextLine();
                     System.out.print("Género do Filme: ");
                     String genero = scan.nextLine();
@@ -47,8 +56,9 @@ public class Main {
                         generoRealizador = scan.nextLine();
                     }
                     Realizador realizador = new Realizador(primeiroNomeRealizador, ultimoNomeRealizador, generoRealizador.equalsIgnoreCase("M"));
-                    Filme f = new Filme(nome, genero, numEdicao, realizador);
-                    System.out.println(f);
+                    Filme f = new Filme(nome, genero, edicaoFilme, realizador);
+                    
+                    //System.out.println(f);
                     break;
                 case "a":
                     System.out.print("NOVO ATOR\nPrimeiro Nome do Ator/Atriz: ");
@@ -67,6 +77,21 @@ public class Main {
                     atores.add(ator);
                     System.out.print(ator);
                     break;
+           //---------------------------------------
+                case "h":
+                    System.out.print("CRIAR EDIÇÃO:\nNúmero de Edição: ");
+                    numEdicao = scan.nextInt();
+                    System.out.print("Ano: ");
+                    ano = scan.nextInt();
+                    Edicao edicao = new Edicao(numEdicao, ano);
+                    edicoes.add(indexEdicoes, edicao);
+                    indexEdicoes++;
+                    break;
+                    
+                case "i":
+                    consultarEdicoes();
+                    break;
+           //--------------------------------------------
                 case "p":
                     int i = 1;
                     for (Ator a : atores) {
@@ -103,6 +128,23 @@ public class Main {
             }
             quebra = true;
         }
+        }
+        
     }
-
+    
+    
+    public static void consultarEdicoes(){
+        int aux = 0;
+        if (edicoes.isEmpty()){
+            System.out.println("Ainda não há nehuma edição do festival!");    
+        }
+        else{
+            while (aux < edicoes.size()){
+            System.out.println(edicoes.get(aux));
+            aux++;
+        }
+        }
+        
+        
+    }
 }
