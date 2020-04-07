15c3290c94053fc78aa1f3ff2aafbb885bc63c70
ProgRub <ruben2000jgr@gmail.com>
2020-04-07 14:52:40 +0100

    ..

--- FestivalCinema/src/main/java/Main.java
+++ FestivalCinema/src/main/java/Main.java
@@ -1,14 +1,9 @@
+
 public class Main {
 
     public static void main(String[] args) {
         FestivalCinema festivalCinema = new FestivalCinema();
         festivalCinema.menu();
-
     }
 
-    
 }
-
-
-
-       
