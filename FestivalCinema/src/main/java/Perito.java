b4832cb6c42fe2b2e36ec2396d6f5261b9c5298e
ProgRub <ruben2000jgr@gmail.com>
2020-04-07 16:37:36 +0100

    Verificação da pontuação e escrita dos candidatos a pontuar

--- FestivalCinema/src/main/java/Perito.java
+++ FestivalCinema/src/main/java/Perito.java
@@ -1,4 +1,6 @@
 
+import java.util.Scanner;
+
 public class Perito extends Pessoa {
 
     public Perito(String nome, boolean genero) {
@@ -10,7 +12,18 @@ public class Perito extends Pessoa {
         return perito;
     }
 
-    public void inserePontuacao(int pontuacao, Premio premio, int indiceCandidato, int indicePerito) {
-        premio.setPontuacao(indiceCandidato, indicePerito, pontuacao);
+    public boolean inserePontuacao(Premio premio, int indiceCandidato, int indicePerito, Scanner scan) {
+        System.out.printf((this.getGenero() ? "O perito %s " : "A perita %s ") + "atribui ao candidato a pontuação (de 1 a 10): ", this.getNome());
+        double pontuacao = scan.nextDouble();
+        scan.nextLine();
+        if (pontuacao > 0 && pontuacao < 10 && pontuacao == (int)pontuacao) {
+            premio.setPontuacao(indiceCandidato, indicePerito, (int)pontuacao);
+        }
+        else
+        {
+            System.out.println("O valor precisa de ser entre 1 e 10 e inteiro!");
+            return false;
+        }
+        return true;
     }
 }
